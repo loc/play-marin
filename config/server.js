fe6ea7f1eb6794d829bc131e795af1c4589d3b2a
loc <andy@slack-corp.com>
2020-12-08 03:13:25 +0000

    Update heroku-deploy to output generated at 6c75052

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d42ed3610e207911474b4a3d2ba0b70'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c237130-ea96-4421-afb8-fafa0f5be9b9'
+};
