1e1b92d89c41890a8ffe342b42968c06ae4590fe
lindsey-s <ly.stevenson@gmail.com>
2021-02-12 15:13:13 -0800

    use next/router to handle DonateButton click navigation

--- frontend/src/components/donate-button.tsx
+++ frontend/src/components/donate-button.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function DonateButton({className}: {className: string}) {
+    const router = useRouter();
     const buttonClass = [buttonStyles.primary, className].join(' ');
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push('/donate')
+    }
     
     return (
-        <button className={buttonClass}>
+        <button className={buttonClass} onClick={handleClick}>
             Donate
         </button>
     )
