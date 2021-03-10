8dfa311e0d58c1e36061a3b320c32f4cc0342955
lindsey-s <ly.stevenson@gmail.com>
2021-03-10 12:46:29 -0800

    styles for thank you page

--- frontend/src/pages/thank-you.tsx
+++ frontend/src/pages/thank-you.tsx
@@ -1,8 +1,16 @@
-import { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
-import { DefaultPageWrap } from '../components/default-page-wrap'
+import Footer from '../components/footer'
+import { fetchApi } from '../utils/api'
+
+import thanks from '../styles/thank-you-page.module.scss'
+import buttons from '../styles/buttons.module.scss'
+
+// move type declaration to ...? somewhere shared
+type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
 
 async function fetchGetJSON(url: string) {
   try {
@@ -13,7 +21,13 @@ async function fetchGetJSON(url: string) {
   }
 }
 
-const ThankYou: NextPage = () => {
+export default function ThankYou({
+  image,
+  imageOverlayText,
+  headerText,
+  bodyText,
+  homeButtonText,
+}: Awaited<ReturnType<typeof getStaticProps>>['props']) {
   const router = useRouter()
 
   const { data, error } = useSWR(
@@ -27,16 +41,75 @@ const ThankYou: NextPage = () => {
 
   const status = data?.session?.payment_intent?.status
 
+  function handleHomeButtonClick() {
+    router.push('/')
+  }
+
   return (
-    <DefaultPageWrap activeMenuItem={'none'}>
-      <div>
-        <h1>Donation Result</h1>
-        <h2>Status: {status ? status : 'loading...'}</h2>
-        <h3>CheckoutSession response:</h3>
-        <pre>{data ? JSON.stringify(data, null, 2) : 'loading...'}</pre>
-      </div>
-    </DefaultPageWrap>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Roboto+Condensed:wght@700&family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+      <main className={thanks['content']}>
+        
+        <div className={thanks['left-container']}>
+          <div  className={thanks['img']}
+                style={{ backgroundImage: `url(${image.url})` }} >
+          </div>
+
+          <div className={thanks['left-content']}>
+            <div className={thanks['home-logo-container']}>  
+              <Link href='/'>
+                <a>
+                  <div className={thanks['home-logo']}>
+                    <img className={thanks['home-logo-img']}src='/img/play-marin-logo-white.png' alt='Play Marin logo'/>
+                  </div>
+                </a>
+              </Link>
+            </div>
+            
+            <img className={thanks['img-overlay']} src={imageOverlayText.url} alt={imageOverlayText.alternativeText} />
+          </div>
+          
+        </div>
+
+        <div className={thanks['right-container']}>
+          <div className={thanks['right-content']}>
+            <div>
+              <h1 className={thanks['header']}>{headerText}</h1>
+              <p className={thanks['body']}>{bodyText}</p>
+            </div>
+            <button className={buttons['primary']} onClick={handleHomeButtonClick}>
+              {homeButtonText}
+            </button>
+          </div>
+        </div>
+
+      </main>
+      <Footer />
+    </>
   )
 }
 
-export default ThankYou
+export async function getStaticProps() {
+  const thankYouPage = await fetchApi('thank-you-page');
+
+  return {
+    revalidate: 60,
+    props: {
+      image: thankYouPage.image,
+      imageOverlayText: thankYouPage.imageOverlayText,
+      headerText: thankYouPage.headerText as string,
+      bodyText: thankYouPage.bodyText as string,
+      homeButtonText: thankYouPage.homeButtonText as string,
+    },
+  }
+}
